833f1485e9266ddd3556141d3c7d2c6e75028871
Haroun <huru@Harouns-MacBook-Pro.local>
2018-10-18 09:20:14 +0200

    added mock facilities and synchronizations

---
+++ src/utils/mock-facilities.js
@@ -0,0 +1,106 @@
+export default {
+  FACILITIES: {
+    lastSync: {
+      id: "1",
+      date: "2018-10-16"
+    },
+    facilities: [
+      {
+        Name: {
+          previousValue: null,
+          newValue: "Anabanda Health Centre"
+        },
+        CommonName: {
+          previousValue: null,
+          newValue: "Anabanda"
+        },
+        Code: {
+          previousValue: null,
+          newValue: "AHC1"
+        },
+        OperationalStatus: {
+          previousValue: "",
+          newValue: ""
+        },
+        RegulatoryStatus: {
+          previousValue: "",
+          newValue: ""
+        },
+        DateOpened: {
+          previousValue: "",
+          newValue: "2018-10-09"
+        },
+        LastUpdated: {
+          previousValue: "",
+          newValue: ""
+        },
+        DHIS2Code: {
+          previousValue: "",
+          newValue: ""
+        },
+        OpenLMISCode: {
+          previousValue: "",
+          newValue: ""
+        },
+        District: {
+          previousValue: "",
+          newValue: "Lilongwe"
+        },
+        Zone: {
+          previousValue: "",
+          newValue: "Central"
+        },
+        isNew: true,
+        isRemoved: false
+      },
+      {
+        Name: {
+          previousValue: null,
+          newValue: "Akesha Health Centre"
+        },
+        CommonName: {
+          previousValue: null,
+          newValue: "Anabanda"
+        },
+        Code: {
+          previousValue: null,
+          newValue: "AHC1"
+        },
+        OperationalStatus: {
+          previousValue: "",
+          newValue: ""
+        },
+        RegulatoryStatus: {
+          previousValue: "",
+          newValue: ""
+        },
+        DateOpened: {
+          previousValue: "",
+          newValue: "2015-10-09"
+        },
+        LastUpdated: {
+          previousValue: "",
+          newValue: ""
+        },
+        DHIS2Code: {
+          previousValue: "",
+          newValue: ""
+        },
+        OpenLMISCode: {
+          previousValue: "",
+          newValue: ""
+        },
+        District: {
+          previousValue: "",
+          newValue: "balaka"
+        },
+        Zone: {
+          previousValue: "",
+          newValue: "Central"
+        },
+        isNew: true,
+        isRemoved: false
+      }
+    ]
+  }
+};
---
+++ src/utils/mock-syncs.js
@@ -0,0 +1,20 @@
+export default {
+  synchronizations: [
+    {
+      synchronizationId: "fqhfqf672hh",
+      totalFacilitiesAdded: "string",
+      totalFacilitiesRemoved: "string",
+      totalFacilitiesUpdated: "number",
+      synchronizationDate: "Date",
+      clientId: "string"
+    },
+    {
+      synchronizationId: "fqhfqf231rh",
+      totalFacilitiesAdded: "string",
+      totalFacilitiesRemoved: "string",
+      totalFacilitiesUpdated: "number",
+      synchronizationDate: "Date",
+      clientId: "string"
+    }
+  ]
+};
