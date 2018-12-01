1152afbdfe45f3e959d1ba8bc70f25f1b63f6b68
mmalumbo <mmalumbo@gmail.com>
2018-12-01 11:29:57 +0200

    added axios get call
    Signed-off-by: mmalumbo <mmalumbo@gmail.com>

--- src/migrations/DHAMIS/Container.js
+++ src/migrations/DHAMIS/Container.js
@@ -4,12 +4,36 @@ import React, { Component } from "react";
 import From from "./Form";
 import Form from "./Form";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import settings from "../../settings";
+import axios from "axios";
 
 class Container extends Component {
   state = {
     migrations: [],
     isFetchingMigrations: false
   };
+
+  async componentDidMount() {
+    this.setState({ isFetchingMigrations: true });
+
+    const {
+      OPENHIM_URL: URL,
+      OPENHIM_PASSWORD: password,
+      OPENHIM_USER: username
+    } = settings;
+
+    const url = `${URL}/interop-manager/migrations/openlmis`;
+
+    const req = await axios({
+      method: "get",
+      url,
+      auth: { username, password },
+      headers: { "Content-Type": "application/json" }
+    }).catch(err => console.log(err));
+
+    const migrations = req ? req.data : [];
+    this.setState({ migrations, isFetchingMigrations: false });
+  }
 
   clickHandler = () => {
     console.log("fetching dhamis migrations");
